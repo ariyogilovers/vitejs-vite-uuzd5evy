9cc665367e5b5b941f003aa4d3a2f501c4d8e54c
ariyogilovers <ariyogi.1000@gmail.com>
2025-08-20 22:18:08 +0700

    v4

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -10,15 +10,29 @@ const makanan = [
     { nama: "Bakso", harga: 18000 }
   ];
 
+
+  interface Menu {
+    id: string;
+    image_url: string;
+    name: string;
+  }  
+
 const Home = () => {
 
     const [darkmode, setDarkmode] = useState<boolean>(false);
+    const [menus, setMenus] = useState([]);
     console.log("darkmode saat ini",darkmode);
 
     useEffect(() => {
       console.log("Use Efect nih bos");
 
     },[darkmode])
+
+    useEffect(() => {
+      fetch('https://wpu-cafe.vercel.app/api/menu')
+        .then((response) => response.json())
+        .then((result) => setMenus(result.data));
+    }, []);
 
     return (
 
@@ -33,6 +47,19 @@ const Home = () => {
         ))}
       </ul>  
       <br/> 
+      <br />
+      <div className={styles.menu}>
+        {menus.map((item: Menu) => (
+          <div className={styles.item} key={item.id}>
+            <img
+              className={styles.image}
+              src={item.image_url}
+              alt={item.name}
+            />
+            {item.name}
+          </div>
+        ))}
+      </div>
       <button onClick={() => setDarkmode(!darkmode)} > {darkmode ? 'lightmode':'darkmode'}</button>         
         </main>
     );
